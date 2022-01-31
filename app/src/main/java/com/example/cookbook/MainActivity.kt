04e67a9b495c2fe348dc2a1af3aa123e934ac1e7
Sanya-PC <samim_08@mail.ru>
2022-01-31 19:40:44 +0300

    Add login,registr,icon

--- app/src/main/java/com/example/cookbook/MainActivity.kt
+++ app/src/main/java/com/example/cookbook/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.cookbook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cookbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bindingStart: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        bindingStart = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(bindingStart.root)
+
+        bindingStart.loginBtn.setOnClickListener{
+            val loginIntent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(loginIntent)
+
+        }
+        bindingStart.registrBtn.setOnClickListener{
+            val registrActivity = Intent(this@MainActivity, RegistrationActivity::class.java)
+            startActivity(registrActivity)
+
+        }
     }
 }
