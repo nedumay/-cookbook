c019559114371c4d6a612c6153aa767731adadba
Sanya-PC <samim_08@mail.ru>
2022-02-04 16:27:40 +0300

    add menu activity

--- app/src/main/java/com/example/cookbook/LoginActivity.kt
+++ app/src/main/java/com/example/cookbook/LoginActivity.kt
@@ -1,11 +1,26 @@
 package com.example.cookbook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cookbook.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var bindingLogin: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        bindingLogin = ActivityLoginBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(bindingLogin.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val loginIntent = Intent(this@LoginActivity, MainMenuActivity::class.java)
+        bindingLogin.guestBtn.setOnClickListener{
+            startActivity(loginIntent)
+        }
     }
 }
