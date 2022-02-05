45ff1b58e90d487945f5eff70793a6ec93e7d59b
Sanya-PC <samim_08@mail.ru>
2022-02-05 16:34:50 +0300

    Firebase login

--- app/src/main/java/com/example/cookbook/LoginActivity.kt
+++ app/src/main/java/com/example/cookbook/LoginActivity.kt
@@ -5,13 +5,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import com.example.cookbook.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 class LoginActivity : AppCompatActivity() {
 
-    lateinit var bindingLogin: ActivityLoginBinding
+    private lateinit var bindingLogin: ActivityLoginBinding
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,6 +35,43 @@ class LoginActivity : AppCompatActivity() {
         val loginIntent = Intent(this@LoginActivity, MainMenuActivity::class.java)
         bindingLogin.guestBtn.setOnClickListener{
             startActivity(loginIntent)
+        }
+        bindingLogin.loginBtn.setOnClickListener {
+            registrUser()
+        }
+    }
+    
+    /** Логин юзера **/
+    private fun registrUser(){
+        val email = bindingLogin.inputLogin.text.toString()
+        val password = bindingLogin.inputPassword.text.toString()
+
+        if(email.isNotEmpty() && password.isNotEmpty()){
+            CoroutineScope(Dispatchers.IO).launch{
+                try {
+                    auth.signInWithEmailAndPassword(email,password).await()
+                    withContext(Dispatchers.Main){
+                        checkLoggedInState()
+                    }
+                } catch (e:Exception){
+                    withContext(Dispatchers.Main){
+                        Toast.makeText(this@LoginActivity, e.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+
+            }
+        }
+
+    }
+
+    private fun checkLoggedInState() {
+        val loginIntent = Intent(this@LoginActivity, MainMenuActivity::class.java)
+        if(auth.currentUser == null){
+            Toast.makeText(applicationContext,R.string.no_log,Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(applicationContext,R.string.logged,Toast.LENGTH_LONG).show()
+            startActivity(loginIntent)
+
         }
     }
 }
--- app/src/main/java/com/example/cookbook/RegistrationActivity.kt
+++ app/src/main/java/com/example/cookbook/RegistrationActivity.kt
@@ -69,5 +69,4 @@ class RegistrationActivity : AppCompatActivity() {
         }
     }
 
-
 }
